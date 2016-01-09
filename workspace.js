f169b1e29fc2e61a15edecceb2c77864c3469ff7
John Lauer <jlauer@chilipeppr.com>
2016-01-09 23:29:57 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- workspace.js
+++ workspace.js
@@ -16,14 +16,14 @@ cpdefine("inline:com-chilipeppr-workspace-nodemcu", ["chilipeppr_ready"], functi
         id: "com-chilipeppr-workspace-nodemcu", // Make the id the same as the cpdefine id
         name: "Workspace / NodeMCU", // The descriptive name of your widget.
         desc: `A ChiliPeppr Workspace that lets you interact with the NodeMCU device.
-        
-        The NodeMCU device is an ESP8266 wifi module with an attached USB serial port bridge
-        like the way Arduinos have an USB FTDI adapter connected to a micro-controller. Secondly,
-        the NodeMCU has the Lua language preloaded onto the ESP8266 so you can easily program
-        the device.
-        
-        This workspace gives you convenience methods for programming the NodeMCU device.
-        `,
+
+The NodeMCU device is an ESP8266 wifi module with an attached USB serial port bridge
+like the way Arduinos have an USB FTDI adapter connected to a micro-controller. Secondly,
+the NodeMCU has the Lua language preloaded onto the ESP8266 so you can easily program
+the device.
+
+This workspace gives you convenience methods for programming the NodeMCU device.
+`,
         url: "(auto fill by runme.js)", // The final URL of the working widget as a single HTML file with CSS and Javascript inlined. You can let runme.js auto fill this if you are using Cloud9.
         fiddleurl: "(auto fill by runme.js)", // The edit URL. This can be auto-filled by runme.js in Cloud9 if you'd like, or just define it on your own to help people know where they can edit/fork your widget
         githuburl: "(auto fill by runme.js)", // The backing github repo
@@ -40,27 +40,27 @@ cpdefine("inline:com-chilipeppr-workspace-nodemcu", ["chilipeppr_ready"], functi
          * The workspace's init method. It loads the Terminal widget and then the SPJS widget.
          */
         init: function() {
-            
+
             var that = this;
 
             $('#' + this.id).removeClass("hidden");
 
             // Load the terminal widget
             this.loadTerminalWidget(function() {
-                
+
                 // now we can load the SPJS widget
                 that.loadSpjsWidget(function() {
-                    
+
                     // if we get here, we can init the Terminal Widget
                     that.widgetTerminal.init();
                     that.widgetTerminal.resize();
-                    
+
                     that.setupResize();
                 })
-                
+
             })
-            
-        }, 
+
+        },
         /**
          * Returns the billboard HTML, CSS, and Javascript for this Workspace. The billboard
          * is used by the home page, the workspace picker, and the fork pulldown to show a
@@ -81,7 +81,7 @@ cpdefine("inline:com-chilipeppr-workspace-nodemcu", ["chilipeppr_ready"], functi
          * When browser window resizes, forcibly resize the Terminal window
          */
         onResize: function() {
-            this.widgetTerminal.resize();    
+            this.widgetTerminal.resize();
         },
         /**
          * Load the Terminal widget via chilipeppr.load()
@@ -100,11 +100,11 @@ cpdefine("inline:com-chilipeppr-workspace-nodemcu", ["chilipeppr_ready"], functi
                             console.log("My widget just got loaded.", mywidget);
 
                             that.widgetTerminal = mywidget;
-                            
+
                             // load spjs widget so we can test
                             //http://fiddle.jshell.net/chilipeppr/vetj5fvx/show/light/
                             callback();
-                            
+
 
                         }
                     );
@@ -115,9 +115,9 @@ cpdefine("inline:com-chilipeppr-workspace-nodemcu", ["chilipeppr_ready"], functi
          * Load the Serial Port JSON Server widget via chilipeppr.load()
          */
         loadSpjsWidget: function(callback) {
-            
+
             var that = this;
-            
+
             chilipeppr.load(
                 "#spjsWidget",
                 "http://fiddle.jshell.net/chilipeppr/vetj5fvx/show/light/",
@@ -129,9 +129,9 @@ cpdefine("inline:com-chilipeppr-workspace-nodemcu", ["chilipeppr_ready"], functi
                         spjs.consoleToggle();
 
                         that.widgetSpjs = spjs;
-                        
+
                         callback();
-                        
+
                     });
                 }
             );
